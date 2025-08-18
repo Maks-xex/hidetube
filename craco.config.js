646fe45fa829c93bc68628f3f291251884c243b1
max <justmax.xex@mail.ru>
2025-08-18 23:34:50 +0200

    feat: add firefox support

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,8 @@
 const path = require("path");
 const StyleLintPlugin = require("stylelint-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const isFirefoxBuild = process.env.BUILD_TARGET === "firefox";
 
 module.exports = {
   webpack: {
@@ -9,6 +12,19 @@ module.exports = {
           context: path.resolve(__dirname, "src"),
           files: ["**/*.css", "**/*.scss"],
           configFile: path.resolve(__dirname, ".stylelintrc.json"),
+        }),
+      );
+
+      webpackConfig.plugins.push(
+        new CopyWebpackPlugin({
+          patterns: [
+            {
+              from: isFirefoxBuild
+                ? path.resolve(__dirname, "src/manifest/manifest.firefox.json")
+                : path.resolve(__dirname, "public/manifest.json"),
+              to: path.resolve(__dirname, "build/manifest.json"),
+            },
+          ],
         }),
       );
 
