0210fe3654d32e11621d76bc157c4f391bee0b96
max <justmax.xex@mail.ru>
2025-08-18 23:40:34 +0200

    fix: check if tab exists before closing iframe tab

--- src/utils/applyToIframe.ts
+++ src/utils/applyToIframe.ts
@@ -4,20 +4,21 @@ import { getVisibility } from "./getVisibility";
 
 export const applyToIframe = (tab: chrome.tabs.Tab) => {
   if (!tab.id) return;
+
   chrome.webNavigation.getAllFrames({ tabId: tab.id }, async (frames) => {
     const embed = ["youtube-nocookie.com/embed", "youtube.com/embed"];
     const ytFrame = frames?.find((frame) => embed.some((sub) => frame.url.includes(sub)));
 
-    if (!ytFrame) {
-      return;
-    }
-    try {
-      if (!tab.id) return;
-      await injectContentScript(tab.id!, [ytFrame.frameId]);
-      const visible = await getVisibility();
-      sendVisibility(tab.id, visible, ytFrame.frameId);
-    } catch (err) {
-      console.warn("Injection failed:", err);
-    }
+    if (!ytFrame) return;
+    chrome.tabs.get(tab.id!, async (existingTab) => {
+      if (chrome.runtime.lastError || !existingTab) return;
+      try {
+        await injectContentScript(tab.id!, [ytFrame.frameId]);
+        const visible = await getVisibility();
+        sendVisibility(tab.id!, visible, ytFrame.frameId);
+      } catch (err) {
+        console.warn("Injection failed:", err);
+      }
+    });
   });
 };
